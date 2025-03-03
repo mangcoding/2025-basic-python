cfd99fae5d66d7a94070a2611ab4e406d42143fd
nugrahaindie <nugraha@indietechsolutions.com>
2025-03-03 14:19:22 +0700

    adding more samples

---
+++ string/indexing.py
@@ -0,0 +1,8 @@
+name = "Budi Budiman"
+
+print(name[3])
+print(name[0:4])
+print(name[5:])
+print(name[-7:])
+print(name[::-1])
+print(len(name))
--- string/split_n_join.py
+++ string/split_n_join.py
@@ -1,6 +1,9 @@
-sentence = "Hello World. Today is a good day"
-"Today is a good day. hello world"
+sentence = '''Tim Hukum KPK meminta penundaan sidang praperadilan jilid II Sekjen PDIP Hasto Kristiyanto terkait penetapan status tersangka. Kuasa hukum Hasto berharap penundaan itu bukan akal-akalan KPK untuk menyelesaikan berkas perkara.'''
 
-words = sentence.split(".")
-words = list(map(lambda x: x.lower().strip(), words))
-print((". ".join(words[::-1])).capitalize())
+sentence_split = sentence.split(' ')
+short_sentence = ' '.join(sentence_split[:6])
+
+print(short_sentence.lower())
+print(short_sentence.upper())
+print(short_sentence.capitalize())
+print(short_sentence.replace("KPK","Komisi Pemberantasan Korupsi"))
